877c2e81ebb18cdfd0a9d5af4e98c11f42952bda
Serf <sergiofavelajr@hotmail.com>
2019-01-23 10:22:22 -0600

    first commit

---
+++ ConsoleApp1/DiceBag.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class DiceBag
+    {
+        public enum Dice : uint
+        {
+
+            D2 = 2,
+            D4 = 4,
+            D6 = 6,
+            D8 = 8,
+            D10 = 10,
+            D12 = 12,
+            D20 = 20,
+            D30 = 30,
+            D50 = 50,
+            D60 = 60,
+            D100 = 100
+        };
+
+        private Random _rngsus;
+
+        public DiceBag()
+        {
+            _rngsus = new Random();
+        }
+
+        private int InternalRoll(uint dice)
+        {
+            return 1 + _rngsus.Next((int)dice);
+        }
+
+        public int Roll(Dice d)
+        {
+            return InternalRoll((uint)d);
+        }
+
+        public int RollWithModifier(Dice dice, uint modifier)
+        {
+            return InternalRoll((uint)dice) + (int)modifier;
+        }
+
+        public List<int> RollQuantity(Dice d, uint times)
+        {
+            List<int> rolls = new List<int>();
+            for (int i = 0; i < times; i++)
+            {
+                rolls.Add(InternalRoll((uint)d));
+            }
+            return rolls;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keypress;
+            int r = 0;
+            int s1 = 0;
+            int s2 = 0;
+            DiceBag bag = new DiceBag();
+            Console.WriteLine("Press enter to let it roll...");
+            while (true) {
+                
+                keypress = Console.ReadKey();
+                if (keypress.Key != ConsoleKey.Enter)
+                    break;
+                Console.Clear();
+                r = bag.Roll(DiceBag.Dice.D100);
+                s1 += r;
+                Console.Write(r);
+                if (r == 100)
+                    Console.WriteLine("\t\t\tP1: Saquese alv compa");
+                if (r == 1)
+                    Console.WriteLine("\t\t\tP2: Pilas compa");
+                r = bag.Roll(DiceBag.Dice.D100);
+                s2 += r;
+                Console.Write("\t" + r + "    \n");
+                if (r == 1)
+                    Console.WriteLine("\t\t\tP1: Pilas compa");
+                if (r == 100)
+                    Console.WriteLine("\t\t\tP2: Saquese alv compa");
+                Console.Write("__\t__\nP1:"+s1+ "\tP2:"+s2);
+                
+               
+            }
+
+        }
+
+       
+    }
+}
